43d84ab06b50d2e0bdb87395ccf662725d535586
owenchen <owehchen@tw.ibm.com@github.ibm.com>
2026-05-20 18:31:18 +0800

    Bob learning draft

--- topics/station-system/src/main/resources/data.sql
+++ topics/station-system/src/main/resources/data.sql
@@ -1,14 +1,46 @@
 -- 高雄捷運車站初始資料
 -- 紅線和橘線的主要車站
 
+-- 紅線 (Red Line)
 INSERT INTO station (code, name, line, created_at) VALUES
-('R3', '高雄車站', '紅線', CURRENT_TIMESTAMP),
-('R4', '美麗島', '紅線', CURRENT_TIMESTAMP),
-('R5', '中央公園', '紅線', CURRENT_TIMESTAMP),
-('R6', '三多商圈', '紅線', CURRENT_TIMESTAMP),
-('O5', '美麗島', '橘線', CURRENT_TIMESTAMP),
-('O7', '文化中心', '橘線', CURRENT_TIMESTAMP),
+('RK1', '岡山車站', '紅線', CURRENT_TIMESTAMP),
+('R24', '岡山高醫', '紅線', CURRENT_TIMESTAMP),
+('R23', '橋頭火車站', '紅線', CURRENT_TIMESTAMP),
+('R22A', '橋頭糖廠', '紅線', CURRENT_TIMESTAMP),
+('R22', '青埔', '紅線', CURRENT_TIMESTAMP),
+('R21', '都會公園', '紅線', CURRENT_TIMESTAMP),
+('R20', '後勁', '紅線', CURRENT_TIMESTAMP),
+('R19', '楠梓科技園區', '紅線', CURRENT_TIMESTAMP),
+('R18', '油廠國小', '紅線', CURRENT_TIMESTAMP),
+('R17', '世運', '紅線', CURRENT_TIMESTAMP),
+('R16', '左營（高鐵）', '紅線', CURRENT_TIMESTAMP),
+('R15', '生態園區', '紅線', CURRENT_TIMESTAMP),
+('R14', '巨蛋', '紅線', CURRENT_TIMESTAMP),
+('R13', '凹子底', '紅線', CURRENT_TIMESTAMP),
+('R12', '後驛', '紅線', CURRENT_TIMESTAMP),
+('R11', '高雄車站', '紅線', CURRENT_TIMESTAMP),
+('R9', '中央公園', '紅線', CURRENT_TIMESTAMP),
+('R8', '三多商圈', '紅線', CURRENT_TIMESTAMP),
+('R7', '獅甲', '紅線', CURRENT_TIMESTAMP),
+('R6', '凱旋', '紅線', CURRENT_TIMESTAMP),
+('R5', '前鎮高中', '紅線', CURRENT_TIMESTAMP),
+('R4A', '草衙', '紅線', CURRENT_TIMESTAMP),
+('R4', '高雄國際機場', '紅線', CURRENT_TIMESTAMP),
+('R3', '小港', '紅線', CURRENT_TIMESTAMP),
+
+-- 橘線 (Orange Line)
+('OT1', '大寮', '橘線', CURRENT_TIMESTAMP),
+('O14', '鳳山國中', '橘線', CURRENT_TIMESTAMP),
+('O13', '大東', '橘線', CURRENT_TIMESTAMP),
+('O12', '鳳山', '橘線', CURRENT_TIMESTAMP),
+('O11', '鳳山西站 (高雄市議會)', '橘線', CURRENT_TIMESTAMP),
+('O10', '衛武營', '橘線', CURRENT_TIMESTAMP),
+('O9', '苓雅運動園區', '橘線', CURRENT_TIMESTAMP),
 ('O8', '五塊厝', '橘線', CURRENT_TIMESTAMP),
-('O10', '衛武營', '橘線', CURRENT_TIMESTAMP);
+('O7', '文化中心', '橘線', CURRENT_TIMESTAMP),
+('O6', '信義國小', '橘線', CURRENT_TIMESTAMP),
+('O4', '前金', '橘線', CURRENT_TIMESTAMP),
+('O2', '鹽埕埔', '橘線', CURRENT_TIMESTAMP),
+('O1', '哈瑪星', '橘線', CURRENT_TIMESTAMP);
 
 -- Made with Bob
